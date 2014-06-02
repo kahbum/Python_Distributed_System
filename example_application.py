745ba522b33c8ae8f8a9971bb7bd09b0ab5e50d5
ogait <t.oliveira.carmo@gmail.com>
2014-06-01 22:38:43 -0300

    Create example_application.py

---
+++ example_application.py
@@ -0,0 +1,15 @@
+from Client import *
+
+if __name__ == '__main__':
+
+	client = ConnectClient('localhost', 6000)
+
+	client.register_module("func_test.py")
+
+	print "cube 3 >> " + repr(client.request_process(['cube', 3]))
+	print "quad 7 >> " + repr(client.request_process(['quad', 7]))
+	print "test2 >> " + repr(client.request_process(['test2']))
+	print "ten_times 7 >> " + repr(client.request_process(['ten_times', 7]))
+	print "mult 7 8 >> " + repr(client.request_process(['mult', 7, 8]))
+	print "test1 >> " + repr(client.request_process(['test1', ]))
+	print "cube 4 >> " + repr(client.request_process(['cube', 4]))
