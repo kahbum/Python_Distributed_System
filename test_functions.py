f60cb4d0cdd08ab7e768b5329856f7276f16ee85
ogait <t.oliveira.carmo@gmail.com>
2014-06-01 22:47:52 -0300

    Update test_functions.py

--- test_functions.py
+++ test_functions.py
@@ -1,17 +1,17 @@
 def test1():
-	return "test1"
+    return "test1"
 
 def test2():
-	return 2
+    return 2
 
 def quad(x):
-	return x*x
+    return x*x
 
 def cube(x):
-	return x*x*x
+    return x*x*x
 
 def ten_times(x):
-	return 10*x
+    return 10*x
 
 def mult(x, y):
-	return x*y
+    return x*y
